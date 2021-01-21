b7aaf898554fddccd36f77e843dac4dd1f6cb7f1
Sophie Gu <sophieqgu@gmail.com>
2021-01-21 05:01:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@
 function findMatching(array_of_drivers, target) {
   return array_of_drivers.filter( driver => driver === target);
 }
+
+function fuzzyMatch(array_of_drivers, target) {
+  return array_of_drivers.filter( driver => driver.toLowerCase() == target.toLowerCase());
+}
