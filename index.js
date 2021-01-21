fef1fcfad198ae43e6783050c2c01f01dbbfcefc
Sophie Gu <sophieqgu@gmail.com>
2021-01-21 05:04:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution here
 function findMatching(array_of_drivers, target) {
-  return array_of_drivers.filter( driver => driver.toLowerCase() === target.toLowerCase);
+  return array_of_drivers.filter( driver => driver.toLowerCase() == target.toLowerCase);
 }
 
 function fuzzyMatch(array_of_drivers, target) {
